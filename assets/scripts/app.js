47a18c5404b7f80e63a67de9b4994d30682fdec0
Trung Hieu <hieu@ltv.dev>
2023-02-20 09:05:14 +0700

    Render list of posts

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,25 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
 //define type data to get
-xhr.responseType = 'Json';
+xhr.responseType = 'json';
 
 //parse json data
 xhr.onload = function () {  
     const listOfPosts = xhr.response;
-    console.log(listOfPosts);
+
+    //render list of posts by using template
+    for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listElement.append(postEl);
+    }
 }
 
 xhr.send();
+
