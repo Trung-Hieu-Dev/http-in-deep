a7b2bd2568e4279a313880e0ed01c76aae480339
Trung Hieu <hieu@ltv.dev>
2023-02-20 08:42:06 +0700

    Another way to get Json data and convert them to javascript

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,12 +2,12 @@ const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
+//define type data to get
+xhr.responseType = 'Json';
+
 //parse json data
 xhr.onload = function () {  
-    // console.log(xhr.response);
-
-    //convert json to javascript
-    const listOfPosts = JSON.parse(xhr.response);
+    const listOfPosts = xhr.response;
     console.log(listOfPosts);
 }
 
