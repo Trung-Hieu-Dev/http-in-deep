2743d21d9899eaf5f74cc6d1e638046cd51ed56a
Trung Hieu <hieu@ltv.dev>
2023-02-20 08:40:01 +0700

    Get Json data and convert them to javascript

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,4 +2,13 @@ const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
+//parse json data
+xhr.onload = function () {  
+    // console.log(xhr.response);
+
+    //convert json to javascript
+    const listOfPosts = JSON.parse(xhr.response);
+    console.log(listOfPosts);
+}
+
 xhr.send();
